94e4d6114a2db816bfddf736a72267f327c97aaa
peanutt150 <peanutt150@gmail.com>
2026-05-31 20:10:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Starting container..."
+echo "Starting..."
 
-# Fix permissions
 chown -R www-data:www-data /var/www/html/var || true
 chmod -R 777 /var/www/html/var || true
 
-echo "Starting PHP-FPM..."
 php-fpm -D
 sleep 2
 
-# Symfony safety
-if [ -f bin/console ]; then
-  echo "Running migrations..."
-  php bin/console doctrine:migrations:migrate --no-interaction --env=prod || true
-fi
-
-echo "Starting Nginx..."
 exec nginx -g "daemon off;"
